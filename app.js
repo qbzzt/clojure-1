87a31d6165131d6ef0cd727162794de97009315a
Ori Pomerantz <qbzzt1@gmail.com>
2016-05-19 23:18:36 +0000

    Trivial code works

--- app.js
+++ app.js
@@ -12,8 +12,23 @@ var express = require('express');
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
 
+
+// Get a Clojure environment
+var cljs = require('clojurescript-nodejs');
+	
+cljs.eval('(ns c (:require clojure.string)) (defn bold [str] (clojure.string/join ["<b>" str "</b>"]))');	
+	
+// console.log("Function definition: " + 
+//	JSON.stringify(cljs.eval("(ns test (:require [clojure.string :as str])) (defn add-fun [a b] (str/join [a '+' b '=' (+ a b)]))")));	
+	
+	
 // create a new express server
 var app = express();
+
+
+app.get("/test", function(req, res) {
+	res.send("The result is: " + cljs.eval('(ns c) (bold "xx")'));
+});
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
